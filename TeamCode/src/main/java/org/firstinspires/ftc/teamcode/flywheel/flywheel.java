387fda451d1aa3f73d74c81d54642661f252c156
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2020-10-17 14:51:02 -0700

    Update flywheel.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheel/flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheel/flywheel.java
@@ -6,12 +6,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class flywheel extends LinearOpMode{
 
-    public DcMotor flywheel;
+    public DcMotor flywheel, flywheel1;
 
     public void runOpMode() throws InterruptedException {
         flywheel = hardwareMap.get(DcMotor.class, "fw");
+        flywheel1 = hardwareMap.get(DcMotor.class, "fw1");
         flywheel.setDirection(DcMotor.Direction.FORWARD);
-
+        
 
 
         telemetry.addData("Init", true);
@@ -24,9 +25,11 @@ public class flywheel extends LinearOpMode{
 
             if(gamepad1.right_trigger>0.1){
                 flywheel.setPower(1);
+                flywheel1.setPower(1);
             }
             else{
                 flywheel.setPower(0);
+                flywheel1.setPower(0);
             }
 
 
