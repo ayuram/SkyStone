a33577f45dea0c6bf49dc469deb7d7fa1bc39199
MatthewSong86 <matthewysong86@gmail.com>
2020-10-21 15:35:14 -0700

    Create intake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Intake;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class intake extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
