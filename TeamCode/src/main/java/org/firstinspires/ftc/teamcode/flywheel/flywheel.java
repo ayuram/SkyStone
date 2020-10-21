8e16afd4058b14d9368ca1aeedb61aa99aeb8b83
MatthewSong86 <matthewysong86@gmail.com>
2020-10-21 15:43:28 -0700

    Update flywheel.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheel/flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheel/flywheel.java
@@ -2,19 +2,22 @@ package org.firstinspires.ftc.teamcode.flywheel;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class flywheel extends LinearOpMode{
 
     public DcMotor flywheel, flywheel1;
-
+    public Servo angler;
     public void runOpMode() throws InterruptedException {
         flywheel = hardwareMap.get(DcMotor.class, "fw");
         flywheel1 = hardwareMap.get(DcMotor.class, "fw1");
-        flywheel.setDirection(DcMotor.Direction.FORWARD);
-        
-
-
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        angler = hardwareMap.get(Servo.class, "angler");
+        //0.25, 0.5, x
+        angler.setPosition(0.25);
         telemetry.addData("Init", true);
         telemetry.update();
 
@@ -30,6 +33,13 @@ public class flywheel extends LinearOpMode{
             else{
                 flywheel.setPower(0);
                 flywheel1.setPower(0);
+            }
+
+            if(gamepad1.x==true && angler.getPosition()>0.35){
+                angler.setPosition(0.25);
+            }
+            else if(gamepad1.x==true && angler.getPosition()<0.35){
+                angler.setPosition(0.5);
             }
 
 
