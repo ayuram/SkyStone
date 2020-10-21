48a1e1f340b1f5c883b09e93f2dcf28e3bc826ac
MatthewSong86 <matthewysong86@gmail.com>
2020-10-21 15:38:17 -0700

    fixed forward back?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/tanBasedLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/tanBasedLinear.java
@@ -55,9 +55,9 @@ public class tanBasedLinear extends LinearOpMode {
             bl.setPower(d2);
             fr.setPower(d2);
             */
-            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = gamepad1.right_stick_x;
+            double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x*0.8;
             double v1 = r * Math.cos(robotAngle) + rightX;
             double v2 = r * Math.sin(robotAngle) - rightX;
             double v3 = r * Math.sin(robotAngle) + rightX;
