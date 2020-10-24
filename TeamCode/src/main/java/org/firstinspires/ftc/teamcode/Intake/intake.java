d21910dfd72c37400fc990bbdae53d1a6bd12d37
billofrights <bill.yuan27@gmail.com>
2020-10-24 15:09:18 -0700

    j

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
@@ -1,10 +1,28 @@
 package org.firstinspires.ftc.teamcode.Intake;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class intake extends LinearOpMode {
-    @Override
+    public DcMotor intakeR, intakeL;
     public void runOpMode() throws InterruptedException {
-    
+        intakeR = hardwareMap.get(DcMotor.class, "intakeR");
+        intakeL = hardwareMap.get(DcMotor.class, "intakeL");
+        intakeR.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while(opModeIsActive()){
+            if(gamepad1.right_trigger > 0.01){
+                intakeL.setPower(gamepad1.right_trigger);
+                intakeR.setPower(gamepad1.right_trigger);
+            }
+            else if(gamepad1.left_trigger > 0.01){
+                intakeL.setPower(-gamepad1.left_trigger);
+                intakeR.setPower(-gamepad1.left_trigger);
+            }
+        }
     }
 }
