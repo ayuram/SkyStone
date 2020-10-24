29badc7a6cfa46754111c3b09ff8dd200a6d3963
MatthewSong86 <matthewysong86@gmail.com>
2020-10-24 14:57:41 -0700

    increase wobble speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.Intake;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class intake extends LinearOpMode {
-    @Override
+
     public void runOpMode() throws InterruptedException {
-    
+
     }
 }
