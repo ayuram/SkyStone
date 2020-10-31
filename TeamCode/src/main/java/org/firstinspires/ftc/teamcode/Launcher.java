a982206387c4aa4fa053e7a46bb8dabef19e36cd
billofrights <bill.yuan27@gmail.com>
2020-10-30 19:35:56 -0700

    j

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -12,14 +12,23 @@ public class Launcher {
     public double height;
     public double theta;
 
+
+
     public void findAngle(double x, double y, double goalX, double goalY, double h){
         double d = Math.hypot(Math.abs(x-goalX), Math.abs(y-goalY));
         double theta;
-        double sintheta = (d+Math.sqrt(d*d + ((4*g*d*d)/(2*V*V)) - 4*h))/-2;
-        if(sintheta > 1 || sintheta < 1){
-            sintheta = sintheta * -1 + 2*d;
+        double sinplus = (d+Math.sqrt(d*d + ((4*g*d*d)/(2*V*V)) - 4*h))/-2;
+        double sinminus = (d-Math.sqrt(d*d + ((4*g*d*d)/(2*V*V)) - 4*h))/-2;
+        if(sinplus > 1 || sinplus < -1){
+            theta = Math.asin(sinminus);
         }
-        theta = Math.asin(sintheta);
+        else if(sinminus > 1 || sinminus < -1){
+            theta = Math.asin(sinplus);
+        }
+        else{
+            theta = Math.min(Math.asin(sinminus), Math.asin(sinplus));
+        }
+
         this.theta = theta;
     }
 }
