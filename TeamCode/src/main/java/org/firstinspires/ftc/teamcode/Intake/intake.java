baa3ba65bfa741d69e5489eea65ad97200cfb236
MatthewSong86 <matthewysong86@gmail.com>
2020-10-31 14:53:27 -0700

    Update intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/intake.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode.Intake;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="intake", group = "LinearOpMode")
 public class intake extends LinearOpMode {
     public DcMotor intakeR, intakeL;
+    public CRServo in1, in2;
     public void runOpMode() throws InterruptedException {
         intakeR = hardwareMap.get(DcMotor.class, "intakeR");
         intakeL = hardwareMap.get(DcMotor.class, "intakeL");
@@ -17,14 +19,20 @@ public class intake extends LinearOpMode {
             if(gamepad1.right_trigger > 0.01){
                 intakeL.setPower(gamepad1.right_trigger);
                 intakeR.setPower(gamepad1.right_trigger);
+                in1.setPower(1);
+                in2.setPower(0);
             }
             else if(gamepad1.left_trigger > 0.01){
                 intakeL.setPower(-gamepad1.left_trigger);
                 intakeR.setPower(-gamepad1.left_trigger);
+                in1.setPower(0);
+                in2.setPower(1);
             }
             else {
                 intakeL.setPower(0);
                 intakeR.setPower(0);
+                in1.setPower(0.5);
+                in2.setPower(0.5);
             }
         }
     }
