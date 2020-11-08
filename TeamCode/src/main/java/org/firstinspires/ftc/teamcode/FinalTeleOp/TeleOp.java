08276d5e28631eb3ec8697261f33d5c80408ab7a
MatthewSong86 <matthewysong86@gmail.com>
2020-11-08 15:26:23 -0800

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp/TeleOp.java
@@ -37,7 +37,7 @@ public class TeleOp extends LinearOpMode{
         flap = hardwareMap.get(Servo.class, "flap");
         tilt = hardwareMap.get(Servo.class, "tilt");
         //0.25, 0.5, x
-        mag.setPosition(1);
+        mag.setPosition(0.5);
         tilt.setPosition(0.5);
         telemetry.addData("Status", "Initialized");
         waitForStart();
@@ -120,8 +120,8 @@ public class TeleOp extends LinearOpMode{
                 int i = 0;
                 while(i<3){
                     if(i == 0) tilt.setPosition(0.55);
-                    mag.setPosition(1);
-                    mag.setPosition(0.7);
+                    mag.setPosition(0.5);
+                    mag.setPosition(0.2);
                     i++;
                     telemetry.addData("i", i);
                     telemetry.update();
